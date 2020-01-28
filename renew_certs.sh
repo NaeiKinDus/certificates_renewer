4ba1b343d199098d6a694995a4d947c440ee0990
NaeiKinDus <naeikindus@0x2a.ninja>
2020-01-28 02:53:53 +0100

    Added support for options, working script

--- renew_certs.sh
+++ renew_certs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -o errexit -o pipefail -o noclobber
+
 # https://stackoverflow.com/a/45201229
 function mfcb {
     local val="$4";
@@ -24,10 +26,87 @@ function val_trim {
     val_rtrim;
 };
 
+function usage() {
+    cat <<EOF
+USAGE
+\$> $0 run domain [ domain ... ]
+\$> $0 renew domain [ ... domain ]
+
+OPTIONS
+-h/--help: show this menu
+-v/--verbose: show more debug messages
+-s/--server: URL to use to connect to ACME server
+-l/--lego-bin: complete path to the lego binary
+-d/--dot-lego-dir: path to the .lego directory (generated when using the \`run\` command
+-t/--dry-run: use the staging ACME server"
+EOF
+}
+
+# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+! getopt --test > /dev/null
+if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+    echo -e "Getopt not available, please install linux-utils or similar package."
+    exit 1
+fi
+
+## Options
+# s, --server: which server to use for ACME
+# v, --verbose: display more information
+# l, --lego-bin: path to the lego binary
+# d, --dot-lego: path to the .lego directory
+OPTIONS=s:vl:d:ht
+LONGOPTS=server:,verbose,lego-bin:,dot-lego:,help,dry-run
+
+! PARSED=$(getopt --options=${OPTIONS} --longoptions=${LONGOPTS} --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    exit 2
+fi
+
+eval set -- "${PARSED}"
+ACME_SERVER="https://acme-v02.api.letsencrypt.org/directory" VERBOSE=0 LEGO_BIN= DOT_LEGO_DIR=
+while true; do
+    case "$1" in
+	-h|--help)
+	    usage
+	    exit 0
+	    ;;
+	-s|--server)
+	    ACME_SERVER="$2"
+	    shift 2
+	    ;;
+	-v|--verbose)
+	    VERBOSE=1
+	    shift
+	    ;;
+	-l|--lego-bin)
+	    LEGO_BIN="$2"
+	    shift 2
+	    ;;
+	-d|--dot-lego)
+	    DOT_LEGO_DIR="$2"
+	    shift 2
+	    ;;
+	-t|--dry-run)
+	    ACME_SERVER="https://acme-staging-v02.api.letsencrypt.org/directory"
+	    shift
+	    ;;
+	--)
+	    shift
+	    break
+	    ;;
+	*)
+	    echo "Unsupported option: $1"
+	    exit 3
+	    ;;
+    esac
+done
+
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+
 if [ -z "${DOT_LEGO_DIR}" ]; then
-    DOT_LEGO_DIR="$(dirname $0)/.lego"
+    DOT_LEGO_DIR="$(dirname ${SCRIPT_PATH})/.lego"
     if [ ! -d "${DOT_LEGO_DIR}" ]; then
-	"$(mkdir -p ${DOT_LEGO_DIR})"
+	mkdir -p ${DOT_LEGO_DIR}
     fi
 fi
 
@@ -79,10 +158,8 @@ if [ "$1" == "hook" ]; then
 	fi
 
 	METHOD_ARGS="$(echo $METHOD_ARGS | envsubst)"
-	echo -e "Executing: ${METHOD} ${METHOD_ARGS}"
-	#$(${METHOD} ${METHOD_ARGS} 2> /dev/null)
+	$(${METHOD} ${METHOD_ARGS} 2> /dev/null)
     done < "$(dirname ${0})/services.dat"
-    
     exit 0
 fi
 
@@ -124,16 +201,16 @@ for DOMAIN in $*
 do
     echo -e "Renewing for domain ${DOMAIN}..."
     if [[ $ACTION == "run" ]]; then
-	"$(GANDIV5_API_KEY=${API_KEY} ${LEGO_BIN} --server https://acme-staging-v02.api.letsencrypt.org/directory --path ${DOT_LEGO_DIR} --domains ${DOMAIN} --accept-tos --email naeikindus@0x2a.ninja --dns gandiv5 run --must-staple)"
-	#if [ $? -eq 0 ]; then
-	echo -e "\tRunning hook for newly created certificates..."
-	"$($0 hook ${DOMAIN})"
-	### Gets an error, file not found, no idea why
-	#else
-	#    >&2 echo -e "\tCould not generate a new certificate, hook will not be executed !"
-	#    continue
-	#fi
+	GANDIV5_API_KEY=${API_KEY} ${LEGO_BIN} --server ${ACME_SERVER} --path ${DOT_LEGO_DIR} --domains ${DOMAIN} --accept-tos --email naeikindus@0x2a.ninja --dns gandiv5 run --must-staple > /dev/null
+	if [ $? -eq 0 ]; then
+	    ${SCRIPT_PATH} hook ${DOMAIN}
+	else
+	    >&2 echo -e "Could not generate a new certificate, hook will not be executed !"
+	    continue
+	fi
     elif [[ $ACTION == "renew" ]]; then
-	"$(GANDIV5_API_KEY=${API_KEY} ${LEGO_BIN} --server https://acme-staging-v02.api.letsencrypt.org/directory --path ${DOT_LEGO_DIR} --domains ${DOMAIN} --accept-tos --email naeikindus@0x2a.ninja --dns gandiv5 renew --must-staple --renew-hook \"$0 hook ${DOMAIN}\" --days 90)"
+	echo -e "Renewing a certificate"
+	GANDIV5_API_KEY=${API_KEY} ${LEGO_BIN} --server ${ACME_SERVER} --path ${DOT_LEGO_DIR} --domains ${DOMAIN} --accept-tos --email naeikindus@0x2a.ninja --dns gandiv5 renew --must-staple --renew-hook "$0 hook ${DOMAIN}" --days 90 > /dev/null
+	echo -e "!! Certificate renewed !!"
     fi
 done
