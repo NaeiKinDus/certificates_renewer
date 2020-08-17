ca2f9c4e8176fc5d175251ac2dd46ec004e6bd6b
NaeiKinDus <naeikindus@0x2a.ninja>
2020-08-17 18:33:56 +0200

    modularized install script

--- install_certificates.sh
+++ install_certificates.sh
@@ -20,83 +20,14 @@ OPTIONS
 -d/--dry-run: print the commands instead of running them
 -c/--cert-name <filename>: name used for the certificate file in destination (must include file extension)
 -k/--key-name <filename>: name used for the private key file in destination (must include file extension)
---nginx-dir <dir>: directory where the certificate files will be stored for nginx
---nginx-user <account_name>: user owning the nginx / website files (usually www-data)
---nginx-group <group_name>: group owning the nginx / website files (usually www-data)
---traefik-dir: directory where the certificate files will be stored for traefik
---traefik-user <account_name>: user owning the configuration files for traefik (usually traefik)
---traefik-group <group_name>: group owning the configuration files for traefik (usually traefik)
 EOF
 }
 
-source ./common.sh
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# shellcheck disable=SC2034
+SCRIPT_PATH=${SCRIPT_DIR}/$(basename "${BASH_SOURCE[0]}")
 
-function cleanup() {
-  verbose_print "Cleaning up..."
-  verbose_print "rm -f ${CERT_FILE}"
-  verbose_print "rm -f ${KEY_FILE}"
-}
-
-#####################################
-# Software related update functions #
-#####################################
-function update_nginx() {
-    quiet_print "Running Nginx update..."
-
-    if [ ! -d "${NGINX_DIR}" ]; then
-	quiet_print "Destination directory ${NGINX_DIR} does not exist !"
-	exit 4
-    fi
-
-    # CRT update
-    copy_files "${CERT_FILE}" "${NGINX_DIR}/${DEST_CERT_FILENAME}"
-    do_chown "${NGINX_USER}" "${NGINX_GROUP}" "${NGINX_DIR}/${DEST_CERT_FILENAME}"
-
-    # Private key update
-    copy_files "${KEY_FILE}" "${NGINX_DIR}/${DEST_KEY_FILENAME}"
-    do_chown "${NGINX_USER}" "${NGINX_GROUP}" "${NGINX_DIR}/${DEST_KEY_FILENAME}"
-
-    # Reloading Nginx
-    verbose_print "Certificate updated, reloading Nginx..."
-    if [[ $DRY_RUN -eq 1 ]]; then
-	quiet_print "/bin/systemctl reload nginx.service"
-    else
-	/bin/systemctl reload nginx.service
-	if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-	    quiet_print "Could not reload nginx, system might be in an unstable state"
-	    exit 4
-	fi
-    fi
-    verbose_print "Success !"
-}
-
-function update_traefik() {
-    quiet_print "Running traefik update..."
-
-    if [ ! -d "${TRAEFIK_DIR}" ]; then
-	quiet_print "Destination directory ${TRAEFIK_DIR} does not exist !"
-	exit 4
-    fi
-
-    # CRT update
-    copy_files "${CERT_FILE}" "${TRAEFIK_DIR}/${DEST_CERT_FILENAME}"
-    do_chown "${TRAEFIK_USER}" "${TRAEFIK_GROUP}" "${TRAEFIK_DIR}/${DEST_CERT_FILENAME}"
-
-    # Private key update
-    copy_files "${KEY_FILE}" "${TRAEFIK_DIR}/${DEST_KEY_FILENAME}"
-    do_chown "${TRAEFIK_USER}" "${TRAEFIK_GROUP}" "${TRAEFIK_DIR}/${DEST_KEY_FILENAME}"
-
-    verbose_print "Certificate updated, reloading Traefik..."
-    if [[ $DRY_RUN -eq 1 ]]; then
-	quiet_print "/bin/systemctl reload traefik.service"
-    else
-	/bin/systemctl restart traefik.service
-	if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-	    quiet_print "Could not restart Traefik, system might be in an unstable state"
-	    exit 4
-	fi
-    fi
-}
+source "${SCRIPT_DIR}/common.sh"
 
 #####################
 # Shell script entry #
@@ -108,7 +39,7 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
 fi
 
 OPTIONS=hvm:e:ds:c:k:q
-LONGOPTS=help,verbose,email:,env:,dry-run,domain:,cert-name:,key-name:,quiet,nginx-dir:,traefik-dir:,nginx-user:,nginx-group:,traefik-user:,traefik-group:
+LONGOPTS=help,verbose,email:,env:,dry-run,domain:,cert-name:,key-name:,quiet
 
 ! PARSED=$(getopt --options=${OPTIONS} --longoptions=${LONGOPTS} --name "$0" -- "$@")
 if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
@@ -129,13 +60,7 @@ MATCH_DOMAIN=${MATCH_DOMAIN:=""}
 DST_CERT_NAME=${DST_CERT_NAME:=""}
 DST_KEY_NAME=${DST_KEY_NAME:=""}
 NO_OUTPUT=${NO_OUTPUT:=0}
-NGINX_DIR=${NGINX_DIR:="/etc/nginx/ssl"}
-TRAEFIK_DIR=${TRAEFIK_DIR:="/home/traefik/ssl"}
-NGINX_USER=${NGINX_USER:="www-data"}
-NGINX_GROUP=${NGINX_GROUP:="www-data"}
-TRAEFIK_USER=${TRAEFIK_USER:="traefik"}
-TRAEFIK_GROUP=${TRAEFIK_GROUP:="traefik"}
-ENV_FILE=${ENV_FILE:=".env"}
+ENV_FILE=${ENV_FILE:="${SCRIPT_DIR}/.env"}
 
 export DRY_RUN
 export EMAIL_CONTACT
@@ -181,30 +106,6 @@ while true; do
 	    NO_OUTPUT=1
 	    shift
 	    ;;
-	--nginx-dir)
-	    NGINX_DIR="${2}"
-	    shift 2
-	    ;;
-	--nginx-user)
-	    NGINX_USER="${2}"
-	    shift 2
-	    ;;
-	--nginx-group)
-	    NGINX_GROUP="${2}"
-	    shift 2
-	    ;;
-	--traefik-dir)
-	    TRAEFIK_DIR="${2}"
-	    shift 2
-	    ;;
-	--traefik-user)
-	    TRAEFIK_USER="${2}"
-	    shift 2
-	    ;;
-	--traefik-group)
-	    TRAEFIK_GROUP="$2"
-	    shift 2
-	    ;;
 	--)
 	    shift
 	    break
@@ -222,19 +123,16 @@ if [ ! -f "${ENV_FILE}" ]; then
   echo -e "Missing .env file, please use the provided example and modify it according to your needs or specify one using the -e flag."
   exit 1
 fi
-source ./.env
+source "${ENV_FILE}"
 
 if [ -z "${1}" ]; then
     quiet_print "Missing <dir> argument."
     exit 1
 fi
 
-# shellcheck disable=SC2086
-CERT_DIR="$(realpath ${1})"
-# shellcheck disable=SC2086
-CERT_FILE="$(find ${CERT_DIR} -iname \*${MATCH_DOMAIN}\*.crt 2> /dev/null)"
-# shellcheck disable=SC2086
-KEY_FILE="$(find ${CERT_DIR} -iname \*${MATCH_DOMAIN}\*.key 2> /dev/null)"
+CERT_DIR="$(realpath "${1}")"
+CERT_FILE="$(find "${CERT_DIR}" -iname \*"${MATCH_DOMAIN}"\*.crt 2> /dev/null)"
+KEY_FILE="$(find "${CERT_DIR}" -iname \*"${MATCH_DOMAIN}"\*.key 2> /dev/null)"
 
 # shellcheck disable=SC2086
 if [[ "$(echo \"${CERT_FILE}\" | wc -w)" -gt 1 ]]; then
@@ -261,6 +159,7 @@ if [ -z "${DST_CERT_NAME}" ]; then
 else
     DEST_CERT_FILENAME="${DST_CERT_NAME}"
 fi
+export DEST_CERT_FILENAME
 
 if [ -z "${DST_KEY_NAME}" ]; then
     # shellcheck disable=SC2086
@@ -268,13 +167,23 @@ if [ -z "${DST_KEY_NAME}" ]; then
 else
     DEST_KEY_FILENAME="${DST_KEY_NAME}"
 fi
+export DEST_KEY_FILENAME
 
+#############################
+# Executing enabled actions #
+#############################
+shopt -s nullglob
+module_executed=0
+quiet_print "executing enabled actions..."
+for action in "${SCRIPT_DIR}"/enabled-actions/*.sh; do
+  module_executed=1
+  verbose_print "- executing ""${action}"
+  source "${action}"
+done
 
-###################
-# Service updates #
-###################
-update_nginx
-update_traefik
+if [ $module_executed -eq 0 ]; then
+  quiet_print "WARNING: no action were called, this script performed absolutely nothing."
+fi
 
-# Cleanup
-cleanup
+quiet_print "done."
+exit 0
