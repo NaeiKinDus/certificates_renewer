2a9e77a873e00cd4e01d1c0c0e452dd618651543
NaeiKinDus <naeikindus@0x2a.ninja>
2020-08-19 00:58:45 +0200

    Added sudo support

--- available-actions/pct_push.sh
+++ available-actions/pct_push.sh
@@ -17,15 +17,23 @@ function pct_push() {
   assert_defined "DST_FILE (\$3)" "${DST_FILE}"
   verbose_print "pct_push: DST_FILE=${DST_FILE}"
 
-  if [[ ${DRY_RUN} -eq 1 ]]; then
-    quiet_print "${PCT_BIN} ${VM_ID} ${SRC_FILE} ${DST_FILE}"
+  if [[ $USE_SUDO -eq 1 ]]; then
+    SUDO_CMD=${SUDO_BIN}
   else
-    ${PCT_BIN} "${VM_ID}" "${SRC_FILE}" "${DST_FILE}"
+    SUDO_CMD=
+  fi
+
+  if [[ ${DRY_RUN} -eq 1 ]]; then
+    quiet_print "${SUDO_CMD} ${PCT_BIN} ${VM_ID} ${SRC_FILE} ${DST_FILE}"
+  else
+    ${SUDO_CMD} "${PCT_BIN}" "${VM_ID}" "${SRC_FILE}" "${DST_FILE}"
     if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-        quiet_print "Failed to execute command '${PCT_BIN} ${SRC_FILE} ${DST_FILE}'"
+        quiet_print "Failed to execute command '${SUDO_CMD} ${PCT_BIN} ${SRC_FILE} ${DST_FILE}'"
         exit 4
     fi
   fi
 }
 
 PCT_BIN=${PCT_BIN:="/usr/sbin/pct"}
+USE_SUDO=${USE_SUDO:=0}
+SUDO_BIN=${SUDO_BIN:="/usr/bin/sudo"}
