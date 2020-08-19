fba7bfaf2bd9851ea60e7a7e101fa72543782769
NaeiKinDus <naeikindus@0x2a.ninja>
2020-08-19 22:39:50 +0200

    Added sudoers example

--- available-actions/nginx.sh
+++ available-actions/nginx.sh
@@ -47,3 +47,11 @@ NGINX_GROUP=${NGINX_GROUP:="www-data"}
 NGINX_DIR=${NGINX_DIR:="/etc/nginx/ssl"}
 USE_SUDO=${USE_SUDO:=0}
 SUDO_BIN=${SUDO_BIN:="/usr/bin/sudo"}
+
+# Sudoers example:
+# Cmnd_Alias CP_NGINX = /bin/cp /opt/certificates_manager/new_certificates/domain.crt /etc/nginx/ssl/domain.crt, /bin/cp /opt/certificates_manager/new_certificates/domain.key /etc/nginx/ssl/domain.key
+# Cmnd_Alias CHMOD_NGINX = /bin/chmod 600 /etc/nginx/ssl/domain.crt, /bin/chmod 600 /etc/nginx/ssl/domain.key
+# Cmnd_Alias CHOWN_NGINX = /bin/chown www-data\:www-data /etc/nginx/ssl/domain.crt, /bin/chown www-data\:www-data /etc/nginx/ssl/domain.key
+# Cmnd_Alias RESTART_NGINX = /bin/systemctl restart nginx.service
+#
+# certificates_manager ALL=NOPASSWD: CP_NGINX, CHMOD_NGINX, CHOWN_NGINX, RESTART_NGINX
